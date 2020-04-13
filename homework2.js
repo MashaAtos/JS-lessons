58c774e92c8746b24f81f9395aedc0246ea7d86b
MashaAtos <34420989+MashaAtos@users.noreply.github.com>
2020-04-13 23:02:48 +0400

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -1,6 +1,10 @@
 1. Insert a number. Print ‘yes’ if the number is prime, ‘no’ otherwise.
 
-
+function isPrime(num){
+  for(let i = 2; i<num; i++)
+  if(num % i == 0) return "yes";
+return "no";
+}
 
 
 2. Given a number n ( n>= 0 ). Print nth Fibonacci number.  (Fibonacci series: 0, 1, 1, 2, 3, 5, 8 …, ak = ak-1 + ak-2)
