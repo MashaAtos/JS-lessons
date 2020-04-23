e97d1aa8119908e5aa5612952a95da78300bc97c
MashaAtos <34420989+MashaAtos@users.noreply.github.com>
2020-04-23 14:19:01 +0400

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -9,14 +9,19 @@ function getEvenNumber(num) {
 2. Given an array of numbers. Write a recursive function to find its minimal positive
 element. (if such element does not exist, return -1)․
 
-function getMinimal(arr) {
-
-    if (arr.length < 2) {
-        return arr[0];
-    }
-  
-     let x = Math.min(arr.shift(), getMinimal(arr))
-     return x;
-
+function getPositiveMinimum(a, b) {
+  if(a >=0 && b >= 0) return Math.min(a, b);
+  if(a < 0 && b < 0) return -1;
+  if(a < 0) return b;
+  return a;
 }
-getMinimal([56, -9, 87, -23, 0, -105, 55, 1,0])
+
+function getMinimum(arr) {
+  if (arr.length < 2) {
+    return arr[0];
+  }
+  
+  let shiftedValue = arr.shift();
+  let calledValue = getMinimum(arr);
+  return getPositiveMinimum(shiftedValue, calledValue);
+}
