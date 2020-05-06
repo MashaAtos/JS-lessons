a44491c3282f5f3438a99ef005a9fad3de3e741b
MashaAtos <34420989+MashaAtos@users.noreply.github.com>
2020-05-07 00:28:40 +0400

    Update homework4.js

--- homework4.js
+++ homework4.js
@@ -1,3 +1,22 @@
+2. Given an object. Invert it (keys become values and values become keys). If there is
+more than key for that given value create an array.
+
+function swap(obj) {
+  return Object.entries(obj).reduce((acc, [key,value]) => {
+    if (acc[value]) {
+      if(Array.isArray(acc[value])) {
+          acc[value].push(key); 
+       }
+      else {
+          acc[value] = [acc[value],key]
+      }      
+    } else {
+      acc[value] = key
+    }
+    return acc;
+  }, {});
+}
+
 7. Create constructor function which instances would be objects with already
 implemented method map (like Array.map) .
 
