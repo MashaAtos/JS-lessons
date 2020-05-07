9cdf05be82b4f82b55cd29b7b11b9f93e0014139
MashaAtos <34420989+MashaAtos@users.noreply.github.com>
2020-05-07 19:19:39 +0400

    Update homework4.js

--- homework4.js
+++ homework4.js
@@ -1,3 +1,23 @@
+1. Given an array. Write a recursive function that removes the first element and returns
+the given array. (without using arr.unshift(),assign second element to first, third element
+to second...)
+
+let arr = [6, 78, ‘n’, 0, 1];
+function removeFirstElement(array) {
+  function recRemoveFirstElement(array, index) {
+    if (index === array.length) {
+      return [];
+    }
+    let arr = recRemoveFirstElement(array, index + 1);
+    if (index !== 0) {
+      arr = [array[index], ...arr];
+    }
+    return arr;
+  }
+  return recRemoveFirstElement(array, 0);
+}
+removeFirstElement(arr);
+
 2. Given an object. Invert it (keys become values and values become keys). If there is
 more than key for that given value create an array.
 
@@ -16,6 +36,17 @@ function swap(obj) {
     return acc;
   }, {});
 }
+
+3. function sort(list){
+
+  let byPercent =
+  list.sort((a, b) => (
+    (a.readStatus === false) - (b.readStatus === false)
+    || b.percent - a.percent
+  ));
+  return byPercent
+  }
+  sort(list)
 
 7. Create constructor function which instances would be objects with already
 implemented method map (like Array.map) .
