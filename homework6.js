93393dda1b36fdc2ce883542bcf6fabd0224b6bc
MashaAtos <34420989+MashaAtos@users.noreply.github.com>
2020-05-18 13:11:40 +0400

    Update homework6.js

--- homework6.js
+++ homework6.js
@@ -1,10 +1,7 @@
 1. Given an array. Determine whether it consists only from unique elements or not.
 
  function hasDuplicates(arr) {
-   if (new Set(arr).size !== arr.length){
-     return "Duplicated elements in list"
- }
-     return "All elements are unique"
+  return new Set(arr).size !== arr.length ? "Duplicated elements in list" : "Unique";
 }
 
 2. Given an array of numbers. Find the sum of numbers’ quadratic which are even.
