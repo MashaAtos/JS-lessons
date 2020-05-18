a5b75ca61132938fe82b7f76a6cda2409b9adeb7
MashaAtos <34420989+MashaAtos@users.noreply.github.com>
2020-05-18 13:32:06 +0400

    Update homework6.js

--- homework6.js
+++ homework6.js
@@ -5,3 +5,12 @@
 }
 
 2. Given an array of numbers. Find the sum of numbers’ quadratic which are even.
+
+
+
+3. Check whether string is palindrome, or not.
+
+function isPalindrome(text) {
+  let lower = text.toLowerCase();
+    return lower == lower.split("").reverse().join("") ? `${lower} is palindrome` : `${lower} is not palindorme`;
+}
